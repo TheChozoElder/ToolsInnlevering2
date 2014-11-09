09e233598bbe36e6fb3b763c61774c3ecd5049c2
Adrian Arnason <struckthenerve@gmail.com>
2014-11-09 18:59:49 +0100

    småkakk

--- UnityStuff/Assets/Scripts/Misc/StatsUpdater.cs
+++ UnityStuff/Assets/Scripts/Misc/StatsUpdater.cs
@@ -1,11 +1,12 @@
-﻿using UnityEngine;
-using System.IO;
+﻿using System.IO;
 using Newtonsoft.Json;
-
+using UnityEngine;
 
 public class Enemy
 {
 	public string Name { get; set; }
+
+    public int Health { get; set; }
 
 	public static string Serialize(Enemy enemy)
 	{
@@ -13,31 +14,28 @@ public class Enemy
 
 		return JsonConvert.SerializeObject(enemy, Formatting.Indented, settings);
 	}
-
 }
 
 public class StatsUpdater : MonoBehaviour
 {
+    const string FileName = @"Assets\Scripts\Misc\stats.json";
+
 	// Use this for initialization
 	void Start ()
 	{
-
-        var fileName = @"Assets\Scripts\Misc\stats.json";
 	    string serialized = "";
         GameObject enemies = GameObject.Find("Enemies");
-
+        
         foreach (Transform child in enemies.transform)
         {
-			var enemy = new Enemy();
-			enemy.Name = child.name;
+            var enemy = new Enemy { Name = child.name };
 
             serialized += Enemy.Serialize(enemy);
         }
-        using (var writer = new StreamWriter(fileName))
+        using (var writer = new StreamWriter(FileName))
         {
             writer.Write(serialized);
         }
-
 	}
 	
 	// Update is called once per frame
