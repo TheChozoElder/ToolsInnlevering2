c66960dd76f578accab424a3c1c502380f5455bd
Adrian Arnason <struckthenerve@gmail.com>
2014-11-09 14:59:14 +0100

    kul fil

---
+++ StatsUpdater.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    private GameObject enemies;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    enemies = GameObject.Find("Enemies");
+	    int count = 0;
+        foreach (Transform child in enemies.transform)
+        {
+            count++;
+            Debug.Log(count + " " + child.name);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
