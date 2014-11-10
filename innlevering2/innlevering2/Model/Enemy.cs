4aa49054ba86a0662241f1de2c986ca4a05a13a0
KarlEvald <wigkar12@nith.no>
2014-11-10 13:44:43 +0100

    Serializing and dezerializing works :3
    Added some TODO's

--- innlevering2/innlevering2/Model/Enemy.cs
+++ innlevering2/innlevering2/Model/Enemy.cs
@@ -1,38 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-
-namespace innlevering2.Model
+﻿namespace innlevering2.Model
 {
-
-	class EnemyList
-	{
-//		[JsonProperty("Enemy")]
-		public List<Enemy> ListOfEnemies { get; set; }
-
-		public string Serialize()
-		{
-			var settings = new JsonSerializerSettings();
-			//settings.Xxx = ...;
-			//settings.Converters.Add(new Xxx());
-
-			// Convert object to json string.
-			return JsonConvert.SerializeObject(ListOfEnemies, Formatting.Indented, settings);;
-		}
-
-		public EnemyList Deserialize(string jsonData)
-		{
-			return JsonConvert.DeserializeObject<EnemyList>(jsonData);
-		}
-
-	}
-
 	class Enemy
 	{
-
 		public string Name { get; set; }
 		public int MaxHealth { get; set; }
 		public int Scale { get; set; }
---
+++ innlevering2/innlevering2/Model/EnemyList.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace innlevering2.Model
+{
+	class EnemyList
+	{
+		//[JsonProperty("Enemy")]
+		public List<Enemy> ListOfEnemies { get; set; }
+
+		/// <summary>
+		/// Serializes current enemylist to file
+		/// </summary>
+		public string Serialize()
+		{
+			//TODO: Fix settings
+			var settings = new JsonSerializerSettings();
+			//settings.Xxx = ...;
+			//settings.Converters.Add(new Xxx());
+
+			//DEBUG
+			PrintList();
+
+			// Convert object to json string.
+			return JsonConvert.SerializeObject(ListOfEnemies, Formatting.Indented, settings);
+		}
+
+		/// <summary>
+		/// Deserializes current enemylist to file
+		/// </summary>
+		public void Deserialize()
+		{
+
+			//TODO: Is this supposed to be here?
+			var path = @"E:\sak\file.json";
+
+			var jsonStream = new StreamReader(path);
+			var jsonString = jsonStream.ReadToEnd();
+			//TODO: end
+
+			ListOfEnemies = JsonConvert.DeserializeObject<List<Enemy>>(jsonString);
+
+			//DEBUG
+			PrintList();
+		}
+
+		/// <summary>
+		/// Writes all names of current enemies in list
+		/// </summary>
+		private void PrintList()
+		{
+			foreach (var enemies in ListOfEnemies)
+			{
+				Console.WriteLine(enemies.Name);
+			}
+		}
+
+	}
+
+}
