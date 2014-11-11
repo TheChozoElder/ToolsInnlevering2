23ef2e42f4a67ccd7379db81f92ec628dbe2f65b
KarlEvald <wigkar12@nith.no>
2014-11-11 12:42:11 +0100

    Information system added! 3 information icons added

--- innlevering2/innlevering2/MainWindow.xaml.cs
+++ innlevering2/innlevering2/MainWindow.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
+using System.Net.Mime;
 using System.Runtime.Serialization;
+using Microsoft.Win32;
 using Newtonsoft.Json;
 using System.Windows;
 using innlevering2.ViewModel;
@@ -20,6 +21,8 @@ namespace innlevering2
 
 		string Path { get; set; }
 
+		private bool DeserializeButtonActive { get; set; }
+
 		EnemyList EnemyList { get; set; }
 		/// <summary>
 		/// Initializes a new instance of the MainWindow class.
@@ -32,15 +35,16 @@ namespace innlevering2
 			//Initialize stuff
 			EnemyList = new EnemyList {ListOfEnemies = new List<Enemy>()};
 			Path = @"E:\sak\file.json";
+			DeserializeButtonActive = true;
 		}
 
 
 		private void ExportButton(object sender, RoutedEventArgs e)
 		{
 
-			if (!EnemyList.ListOfEnemies.Any())
+			if (EnemyList.ListOfEnemies.Count <= 0)
 			{
-				Console.WriteLine("Nothing to serialize!");
+				SetInfoText("Nothing to export!");
 				return;
 			}
 
@@ -48,7 +52,7 @@ namespace innlevering2
 			{
 				writer.Write((EnemyList.Serialize()));
 			}
-
+			SetInfoText("All data exported");
 		}
 
 		private void ImportButton(object sender, RoutedEventArgs e)
@@ -60,6 +64,79 @@ namespace innlevering2
 			EnemyList.Deserialize(jsonString);
 
 			jsonStream.Close();
+			SetInfoText("All data imported");
+		}
+
+		private void ChangePath(object sender, RoutedEventArgs e)
+		{
+
+			var dlg = new Microsoft.Win32.OpenFileDialog {DefaultExt = ".json", Filter = "JSON Files (*.json)|*.json"};
+
+			var result = dlg.ShowDialog();
+
+
+			if (result != true) return;
+
+			var jsonStream = new StreamReader(dlg.FileName);
+			var jsonString = jsonStream.ReadToEnd();
+
+			try {
+				var tempList = new EnemyList { ListOfEnemies = new List<Enemy>() };
+				tempList.Deserialize(jsonString);
+
+				SetInfoText("File has right format and has deserialized successfully!");
+				DeserializeButtonActive = true;
+
+			} catch (Exception) {
+				DeserializeButtonActive = false;
+				SetInfoText("Warning! Json file has wrong format.");
+			}
+
+			jsonStream.Close();
+
+			Path = dlg.FileName;
+		}
+
+		private void SetInfoText(string infoText = "")
+		{
+			if (DeserializeButtonActive && !infoText.Equals(""))
+			{
+				Info.Text = infoText;
+			}
+			else if (DeserializeButtonActive && infoText.Equals(""))
+			{
+				Info.Text = "All systems GO!";
+			}
+			else
+			{
+				Info.Text = "Chosen file format is not supported, importing will be disabled.";
+			}
+		}
+
+
+		private void EnterSerializeButton(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			if (EnemyList.ListOfEnemies.Count == 0)
+			{
+				SetInfoText("Nothing to export..");
+			} else {
+				SetInfoText("Serializes current data. Exporting to: " + Path);
+				
+			}
+		}
+
+		private void EnterPathButton(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			SetInfoText("Load another file. Current file: " + Path);
+		}
+		private void EnterDeserializeButton(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			SetInfoText("Deserializes current data. Importing from: " + Path);
+		}
+
+		private void LeaveField(object sender, System.Windows.Input.MouseEventArgs e)
+		{
+			SetInfoText();
 		}
 	}
 }
