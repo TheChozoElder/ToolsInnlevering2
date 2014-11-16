8b663657e2caaaaf82377eaca7e6b88568013782
Adrian Arnason <struckthenerve@gmail.com>
2014-11-16 22:30:23 +0100

    Filer til Unity

---
+++ UnityStuff/Assets/Scripts/StatsUpdater/StatsObject.cs
@@ -0,0 +1,24 @@
+﻿namespace innlevering2.Model
+{
+    using System;
+
+    public class StatsObject
+    {
+        public string Name { get; set; }
+
+        public float ScaleX { get; set; }
+        public float ScaleY { get; set; }
+        public float ScaleZ { get; set; }
+
+        public float MaxHealth { get; set; }
+        public float Health { get; set; }
+        public float RegenerateSpeed { get; set; }
+        public bool Invincible { get; set; }
+
+        public float MovementSpeed { get; set; }
+        public float TurningSpeed { get; set; }
+        public float AimingSpeed { get; set; }
+
+        public int InstanceID { get; set; }
+    }
+}
---
+++ UnityStuff/Assets/Scripts/StatsUpdater/StatsObjectList.cs
@@ -0,0 +1,51 @@
+﻿namespace innlevering2.Model
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class StatsObjectList
+    {
+        //Player / objects with unique name
+        public List<StatsObject> NamedEntities { get; set; }
+
+        //Objects who share name with at least one other object
+        public List<StatsObject> UnnamedEntities { get; set; }
+
+        /// <summary>
+        /// Serializes this statsObjectList
+        /// </summary>
+        public string Serialize()
+        {
+            var settings = new JsonSerializerSettings();
+
+            // Convert object to json string.
+            return JsonConvert.SerializeObject(this, Formatting.Indented, settings);
+        }
+
+        /// <summary>
+        /// Deserializes JSON data file and updates itself based on the deserialized data
+        /// </summary>
+        public void Deserialize(string jsonData)
+        {
+            var statsObjectList = JsonConvert.DeserializeObject<StatsObjectList>(jsonData);
+            NamedEntities = statsObjectList.NamedEntities;
+            UnnamedEntities = statsObjectList.UnnamedEntities;
+        }
+
+        /// <summary>
+        /// Enumerator will iterate through both lists in the class, first NamedEntities, then UnnamedEntites
+        /// </summary>
+        /// <returns></returns>
+        public System.Collections.IEnumerator GetEnumerator()
+        {
+            foreach (var namedEntity in NamedEntities)
+            {
+                yield return namedEntity;
+            }
+            foreach (var unnamedEntity in UnnamedEntities)
+            {
+                yield return unnamedEntity;
+            }
+        }
+    }
+}
