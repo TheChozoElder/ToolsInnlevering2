83972a54be6bcfa63b8b0f484017a37fb8a79354
Adrian Arnason <struckthenerve@gmail.com>
2014-11-16 22:20:10 +0100

    Vi hadde visst ikke symbolic link

--- innlevering2/innlevering2/Model/StatsObjectList.cs
+++ innlevering2/innlevering2/Model/StatsObjectList.cs
@@ -1,38 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-
-namespace innlevering2.Model
+﻿namespace innlevering2.Model
 {
-	public class StatsObjectList
-	{
-		//[JsonProperty("Enemy")]
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+
+    public class StatsObjectList
+    {
+        //Player / objects with unique name
         public List<StatsObject> NamedEntities { get; set; }
+
+        //Objects who share name with at least one other object
         public List<StatsObject> UnnamedEntities { get; set; }
 
-		/// <summary>
-		/// Serializes current enemylist to file
-		/// </summary>
-		public string Serialize()
-		{
-			//TODO: Fix settings
-			var settings = new JsonSerializerSettings();
-			//settings.Xxx = ...;
-			//settings.Converters.Add(new Xxx());
+        /// <summary>
+        /// Serializes this statsObjectList
+        /// </summary>
+        public string Serialize()
+        {
+            //TODO: Fix settings
+            var settings = new JsonSerializerSettings();
 
-			// Convert object to json string.
-			return JsonConvert.SerializeObject(this, Formatting.Indented, settings);
-		}
+            // Convert object to json string.
+            return JsonConvert.SerializeObject(this, Formatting.Indented, settings);
+        }
 
-		/// <summary>
-		/// Deserializes current enemylist to file
-		/// </summary>
-		public void Deserialize(string jsonData)
-		{
-			var sOL = JsonConvert.DeserializeObject<StatsObjectList>(jsonData);
-		    this.NamedEntities = sOL.NamedEntities;
-			this.UnnamedEntities = sOL.UnnamedEntities;
-		}
-	}
+        /// <summary>
+        /// Deserializes JSON data file and updates itself based on the deserialized data
+        /// </summary>
+        public void Deserialize(string jsonData)
+        {
+            var statsObjectList = JsonConvert.DeserializeObject<StatsObjectList>(jsonData);
+            NamedEntities = statsObjectList.NamedEntities;
+            UnnamedEntities = statsObjectList.UnnamedEntities;
+        }
 
+        /// <summary>
+        /// Enumerator will iterate through both lists in the class, first NamedEntities, then UnnamedEntites
+        /// </summary>
+        /// <returns></returns>
+        public System.Collections.IEnumerator GetEnumerator()
+        {
+            foreach (var namedEntity in NamedEntities)
+            {
+                yield return namedEntity;
+            }
+            foreach (var unnamedEntity in UnnamedEntities)
+            {
+                yield return unnamedEntity;
+            }
+        }
+    }
 }
